b90b08c007c5ef7bab2b2d9adceeb806eed0fe46
suruirui <guimei201314@gmail.com>
2015-06-06 09:11:14 +0800

    Revert "test"
    This reverts commit 7b075f2d3abc964bba9e8a46f60aeb5cf67551c6.

--- ctors.cpp
+++ ctors.cpp
@@ -6,6 +6,5 @@ int main(){
       cout << "hello github" << endl;
         cout << "hello github" << endl;
           cout << "hello .......github" << endl;
-		  cout << "这样是不是会好一点。。"<< endl;
   return 0;
 }
