2017d8847040d41eeb1fff77a07ee2816f6b0118
suruirui <guimei201314@gmail.com>
2015-06-06 16:58:26 +0800

    qt
    qt

--- ctors.cpp
+++ ctors.cpp
@@ -6,5 +6,7 @@ using namespace std;
 int main(){
 	vector<int> iv(10,0);
 	copy(iv.begin(),iv.end(),ostream_iterator(cout," "));
+
+    cout << "git hub"<<endl;
 	return 0;
 }
---
+++ ctors/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello ctors!" << endl;
+    cout << "Hello ctors!" << endl;
+    cout << "Hello ctors!" << endl;
+    cout << "Hello ctors!" << endl;
+    return 0;
+}
+
