7b075f2d3abc964bba9e8a46f60aeb5cf67551c6
suruirui <guimei201314@gmail.com>
2015-06-06 09:07:52 +0800

    test
    test

--- ctors.cpp
+++ ctors.cpp
@@ -6,5 +6,6 @@ int main(){
       cout << "hello github" << endl;
         cout << "hello github" << endl;
           cout << "hello .......github" << endl;
+		  cout << "这样是不是会好一点。。"<< endl;
   return 0;
 }
